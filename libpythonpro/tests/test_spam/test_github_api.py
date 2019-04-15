547e0e3dc2458c791fa456ce207a8a51ed297d9e
gelhen <gelhen@gmail.com>
2019-04-14 21:58:26 -0300

    Modulo testes concluído

---
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -0,0 +1,27 @@
+from unittest.mock import Mock
+
+import pytest
+
+from libpythonpro import github_api
+
+
+@pytest.fixture()
+def avatar_url(mocker):
+    resp_mock = Mock()
+    url = 'https://avatars1.githubusercontent.com/u/16453890?v=4'
+    resp_mock.json.return_value = {
+        'login': 'gelhen', 'id': 16453890,
+        'node_id': 'MDQ6VXNlcjE2NDUzODkw',
+        'avatar_url': url
+    }
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('gelhen')
+    assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('gelhen')
+    assert 'https://avatars1.githubusercontent.com/u/16453890?v=4' == url
